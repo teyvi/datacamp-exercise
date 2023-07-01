32dfbd344eaae9396249c6d2d00426914971e6f4
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-01 23:23:30 +0000

    functions
    first function

---
+++ functions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 12:06:58 2023
+
+@author: angelateyvi
+"""
+
+def square(value):
+    '''Returns the square of any number'''
+    new_value : value ** 2
+    return new_value
+num = square(4)
+print (num)
