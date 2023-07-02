e0fed99c4ab82bed484e3b22b3f579b1505833e9
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-02 18:28:09 +0000

    single parameter function

---
+++ single-parameter-function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 18:27:24 2023
+
+@author: angelateyvi
+"""
+
+# Define shout with the parameter, word
+def shout(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout('congratulations')
