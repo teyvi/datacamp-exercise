ded10abd54a6268bd366347fd0c046c8ef2c4180
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-02 19:12:08 +0000

    twitter function

---
+++ language-twitter-function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 19:06:43 2023
+
+@author: angelateyvi
+"""
+
+#import pandas library
+import pandas as pd
+
+#import the tweet data frame
+df = pd.read_csv("tweets.csv")
+print(df)
