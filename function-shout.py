6fba998fbed376f28e8b3869eee83c06cd953365
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-02 18:25:17 +0000

    simple function called shout

---
+++ function-shout.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 18:23:53 2023
+
+@author: angelateyvi
+"""
+
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = "congratulations" + "!!!"
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
