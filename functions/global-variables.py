bddde31872dceb1cada1aa962bcde38cd2e6f598
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-03 03:53:33 +0000

    nested funtions

--- functions/global-variables.py
+++ functions/global-variables.py
@@ -6,3 +6,26 @@ Created on Mon Jul  3 03:32:52 2023
 @author: angelateyvi
 """
 
+#global variables are created in the script. They can be changed and altered in a function too
+#this excercise alters the value of the variable defined in the global scope
+
+# Create a string: team
+team = "teen titans"
+
+# Define change_team()
+def change_team():
+    """Change the value of the global variable team."""
+
+    # Use team in global scope
+    global team
+
+    # Change the value of team in global: team
+    team = 'justice league'
+# Print team
+print(team)
+
+# Call change_team()
+change_team()
+
+# Print team
+print(team)
---
+++ functions/nested-functions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 03:50:38 2023
+
+@author: angelateyvi
+"""
+
