3f6d10779576cf9551ecd8755bc22fcd4989a819
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-03 04:59:49 +0000

    functions with multiple argument

---
+++ functions/functions-multiple-argument.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 04:58:38 2023
+
+@author: angelateyvi
+"""
+
+# Define shout_echo
+def shout_echo(word1, intense, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Make echo_word uppercase if intense is True
+    if intense is True:
+        # Make uppercase and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word_new
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo("Hey", echo=5, intense = True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo("Hey", intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
