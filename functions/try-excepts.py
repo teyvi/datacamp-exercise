975e7e3f459df9bb6b978680a2e5f44f91b85866
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-10 00:36:43 +0000

    Create try-excepts.py

---
+++ functions/try-excepts.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 00:36:14 2023
+
+@author: angelateyvi
+"""
+
+# Define count_entries()
+def count_entries(df, col_name='lang'):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: cols_count
+    cols_count = {}
+
+    # Add try block
+    try:
+        # Extract column from DataFrame: col
+        col = df[col_name]
+        
+        # Iterate over the column in DataFrame
+        for entry in col:
+    
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
+    
+        # Return the cols_count dictionary
+        return cols_count
+
+    # Add except block
+    except:
+        'The DataFrame does not have a ' + col_name + ' column.'
+        
+
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
+
+# Print result1
+print(result1)
