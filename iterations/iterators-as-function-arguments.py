ee3f9cf55f0c310b2a21c29796fd8c99e387d89e
Teyvi Angela <44750503+teyvi@users.noreply.github.com>
2023-07-10 23:49:34 +0000

    iterators as function arguments

---
+++ iterations/iterators-as-function-arguments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 23:48:20 2023
+
+@author: angelateyvi
+"""
+
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
